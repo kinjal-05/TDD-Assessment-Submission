2e2c72c5b7ae3dafd4e75ef23059cf97a4fb7d4c
kinjal-05 <kinjalmistry125@gmail.com>
2025-07-04 01:00:41 +0530

    [Add Step 1] Added Failing step where string contains single number

--- Test/stringCalc.test.js
+++ Test/stringCalc.test.js
@@ -3,3 +3,7 @@ const {add} = require('../lib/stringCalc');
 test('Output for Empty string', ()=>{
     expect(add('')).toBe(0);
 });
+
+test('Output for String itself is Number', ()=>{
+    expect(add('1')).toBe(1);
+});
