afcdf43fd4a4368dd2bc60d370096c10c391c6c6
kinjal-05 <kinjalmistry125@gmail.com>
2025-07-04 01:04:11 +0530

    [Add Step 1] Added + Passed where string contains single number

--- Test/stringCalc.test.js
+++ Test/stringCalc.test.js
@@ -7,3 +7,7 @@ test('Output for Empty string', ()=>{
 test('Output for String itself is Number', ()=>{
     expect(add('1')).toBe(1);
 });
+
+test('Output for String itself is Number', ()=>{
+    expect(add('2')).toBe(2);
+});
